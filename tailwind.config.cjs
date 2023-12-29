fb59e303c739d388f0b9f56a3136bf351c9f4671
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2023-12-29 20:18:48 +0300

    font fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Helvetica Neue", "Arial", "Lucida Grande", "sans-serif"],
+    },
   },
   plugins: [],
 };
+
