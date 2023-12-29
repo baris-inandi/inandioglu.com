348caac774d497b4bd06e5be1607179cae898490
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2023-12-29 19:49:47 +0300

    analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://inandioglu.com",
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [mdx(), sitemap(), tailwind()],
 });
+
