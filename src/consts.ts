ce4cb6d45ad27a6ef54280ea40fca23c43868a82
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2024-03-11 18:55:22 +0300

    favicon

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Barış İnandıoğlu";
+export const SITE_TITLE = "Baris Inandioglu";
 export const SITE_DESCRIPTION = "Hello! Welcome to my home page";
+
